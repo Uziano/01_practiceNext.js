6ffcdd42ce7a49105ad10e37f6f2681235c464b5
Uziano <lucianoperaltadefazio@gmail.com>
2022-09-10 13:16:34 -0300

    1est commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About Page</h1>
+            <p>EXample loreme alenalndfa alnfalnfa aljfsnalsnf</p>
+        </div>
+     );
+}
+ 
+export default About;
---
+++ pages/uzis/index.js
@@ -0,0 +1,9 @@
+const Uzis = () => {
+    return ( 
+        <div>
+            <h1>All Uzia</h1>
+        </div>
+    );
+}
+ 
+export default Uzis;
